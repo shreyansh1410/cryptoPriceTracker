85f54b49e1fa8534c45af3383226f640eff12224
Shreyansh Shukla <shreyansh.14010@gmail.com>
2025-01-10 20:33:06 +0530

    added workflow to keep server active

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require("./src/config/db"); // Import the database config
-const cryptoRoutes = require("./src/routes/cryptoRoutes"); // Import routes
-const errorHandler = require("./src/middlewares/errorHandler"); // Import error handling middleware
-const { fetchCryptoDataOnce } = require("./src/cron/cronjob");
+const connectDB = require("./config/db"); // Import the database config
+const cryptoRoutes = require("./routes/cryptoRoutes"); // Import routes
+const errorHandler = require("./middlewares/errorHandler"); // Import error handling middleware
+const { fetchCryptoDataOnce } = require("./cron/cronjob");
 
 dotenv.config();
 
@@ -17,6 +17,10 @@ app.use(errorHandler); // Use error handling middleware
 fetchCryptoDataOnce();
 
 const PORT = process.env.PORT || 5000;
+
+app.get("/ping", (req, res) => {
+  res.status(200).send("Server is active");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
